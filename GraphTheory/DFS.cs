fe5e1d9354d574e8c3d17b066acfa71ca36948a3
Elif Tugce Tekerlekci <tugcetekerlekci@gmail.com>
2019-11-12 16:24:54 -0500

    Update DFS.cs

--- GraphTheory/DFS.cs
+++ GraphTheory/DFS.cs
@@ -1,30 +1,27 @@
 public class DFS<T>
 {
-	  private readonly Vertex<T> root;
-    private readonly HashSet<Vertex<T>> visited = new HashSet<Vertex<T>>();
-    public DFS(Vertex<T> rootVertex)
-    {
-        root = rootVertex;
-    }
+	private readonly Vertex<T> root;
+	private readonly HashSet<Vertex<T>> visited = new HashSet<Vertex<T>>();
+    
+	public DFS(Vertex<T> rootVertex)
+    	{
+        	root = rootVertex;
+    	}
 	
-	  public IEnumerable<Vertex<T>> Iterate()
-    {
-        visited.Clear();
-
-        return DepthFirstSearch(root, visited);
-    }
+	public IEnumerable<Vertex<T>> Iterate()
+    	{
+        	visited.Clear();
+        	return DepthFirstSearch(root, visited);
+    	}
 	
-	  private IEnumerable<Vertex<T>> DepthFirstSearch(Vertex<T> vertex, HashSet<Vertex<T>> visited)
-    {
-        if (visited.Contains(vertex))
-            yield break;
-
-        visited.Add(vertex);
-
-        yield return vertex;
-
-        foreach (Vertex<T> neighbor in vertex.Neighbors.SelectMany(n => DepthFirstSearch(n, visited)))
-            yield return neighbor;
-    }
+	private IEnumerable<Vertex<T>> DepthFirstSearch(Vertex<T> vertex, HashSet<Vertex<T>> visited)
+    	{
+        	if (visited.Contains(vertex))
+            	yield break;
+        	visited.Add(vertex);
+	        yield return vertex;
+        	foreach (Vertex<T> neighbor in vertex.Neighbors.SelectMany(n => DepthFirstSearch(n, visited)))
+            	yield return neighbor;
+    	}
 	
 }
